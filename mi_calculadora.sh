b76307a1b2e5a163f36ec31fba0d151ebbc2e8d5
sangoaku <ascresan7552@iesbezmiliana.net>
2024-12-04 10:52:18 +0100

    testeo

---
+++ mi_calculadora.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source funciones_math.sh
+
+num1=$1
+num2=$2
+op=$3
+
+	if [[ $# -ne 3 ]]; then
+		echo "Introduce 3 argumentos num1 num2 s/r"
+		exit 1
+	fi
+
+	case $op in
+		s)
+			suma "$num1" "$num2"
+		;;
+		r)
+			resta "$num1" "$num2"
+		;;
+		*)
+			echo "Opción no válida introduce s o r"
+		;;
+	esac
